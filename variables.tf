4309b5cb3540a03f5cfdf118e7776ea08d685ca6
jayaprabhuD <119481928+jayaprabhuD@users.noreply.github.com>
2025-06-05 10:00:10 +0000

    Fixing error

--- variables.tf
+++ variables.tf
@@ -9,6 +9,14 @@ variable "sns_topic_name"     { default = "picture-topic" }
 variable "lambda_function_name" { default = "lambda-picture" }
 variable "resize_width" { default = 600 }
 
+variable "tags" {
+  type = map(string)
+  default = {
+    Project   = "ImageProcessor"
+    Owner     = "prodTeam"
+    ManagedBy = "Terraform"
+  }
+}
 variable "email" { 
   default = "jayaprabhu.dheen@hcltech.com"
 }
