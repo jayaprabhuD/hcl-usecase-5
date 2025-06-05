5f1ac1a14416395030a7ed839e6c65564c11a8e4
jayaprabhuD <119481928+jayaprabhuD@users.noreply.github.com>
2025-06-05 10:45:24 +0000

    Fixing error

--- variables.tf
+++ variables.tf
@@ -5,18 +5,20 @@ variable "image-compressed-bucket-name" {
   default = "image-compressed-bucket-name"
 }
 
-variable "sns_topic_name"     { default = "picture-topic" }
+variable "sns_topic_name" { 
+  default = "picture-topic" 
+}
 variable "lambda_function_name" { default = "lambda-picture" }
 variable "resize_width" { default = 600 }
 
-variable "tags" {
-  type = map(string)
-  default = {
-    Project   = "ImageProcessor"
-    Owner     = "prodTeam"
-    ManagedBy = "Terraform"
-  }
-}
+#variable "tags" {
+#  type = map(string)
+#  default = {
+#    Project   = "ImageProcessor"
+#    Owner     = "prodTeam"
+#    ManagedBy = "Terraform"
+#  }
+#}
 variable "email" { 
   default = "jayaprabhu.dheen@hcltech.com"
 }
