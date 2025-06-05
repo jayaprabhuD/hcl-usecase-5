aa4ee68f5eb478aa74c076e672bee865f71e4477
jayaprabhuD <119481928+jayaprabhuD@users.noreply.github.com>
2025-06-05 07:23:55 +0000

    s3-commit

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "upload" {
   bucket = var.upload_bucket_name
-  versioning {
+  aws_s3_bucket_versioning {
     enabled = true
   }
- 
-  server_side_encryption_configuration {
+
+   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
